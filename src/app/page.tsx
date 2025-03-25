5ca9288a84413c795b04a406269dcd4fd8516305
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-03-25 18:18:50 -0400

    Fixed minor bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     
       <div className="grid grid-cols-1 justify-items-center p-8">
-        <div className="flex space-x-10 items-center mx-auto">
+        <div className="grid grid-cols-1 justify-items-center">
           <Image className="rounded-2xl" src="/images/logo_PYME_Movilnet.jpg" alt="Logo PYME Movilnet" width={200} height={100}/>
           <h1 className="text-3xl">Ruleta de la Suerte Movilnet</h1>
         </div>
